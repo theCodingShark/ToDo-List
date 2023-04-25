bf0da037c08c1267e6d8f324e5d7e2c74f924514
theCodingShark <111047102+theCodingShark@users.noreply.github.com>
2023-04-25 13:16:04 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -142,14 +142,8 @@ if(port == null || port == "") {
     port = 3000;
 }
 
-// app.listen(port, function() {
-//     console.log("Server has started successfully.");
-// });
-
-
-mongoose.connect("mongodb+srv://admin-swyam:myTestDatabase@cluster0.ucmfhbg.mongodb.net/todolistDB").then(
-    app.listen(port, function() {
+app.listen(port, function() {
     console.log("Server has started successfully.");
-})
-);
+});
+
 
